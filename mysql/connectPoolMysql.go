3cb2a8f07f6e6eba5c5d33a7789c071706391a6b
likecheng <likecheng@vip.qq.com>
2022-01-13 16:57:49 +0800

    fix bug

--- mysql/connectPoolMysql.go
+++ mysql/connectPoolMysql.go
@@ -25,6 +25,7 @@ func initConnectPoolMysql() {
 	if connectPool == nil {
 		fmt.Println("initConnectPoolMysql +++++++++++")
 		connectPool = new(connectPoolMysql)
+		connectPool.dbMap = make(map[string]*sql.DB, 0)
 	}
 }
 

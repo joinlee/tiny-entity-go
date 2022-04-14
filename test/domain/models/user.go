7f1d0fc3ac0bd346af72416a585138ab6c476e50
likecheng <likecheng@vip.qq.com>
2022-04-14 23:16:18 +0800

    doc

--- test/domain/models/user.go
+++ test/domain/models/user.go
@@ -6,7 +6,8 @@ type User struct {
 	tiny.IEntityObject[User]
 	Id          string        `tiny:"primaryKey" json:"id"`
 	Name        string        `tiny:"varchar(255);notNull;index" json:"name"`
-	Phone       string        `tiny:"varchar(255);index" json:"phone"`
+	Phone       string        `tiny:"varchar(255);default:123" json:"phone"`
+	Age         *int          `tiny:"int(10)" json:"age"`
 	AccountId   string        `tiny:"varchar(32);index" json:"accountId"`
 	Account     *Account      `tiny:"mapping:Account" json:"account"`
 	UserAddress []UserAddress `tiny:"mapping:UserAddress" json:"userAddress"`
